d62dd27384528ec054509b5f862867d3d791e7ac
Clemssandra2939 <sclems600@gmail.com>
2024-12-12 05:15:43 +0100

    Functions with outputs

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Functions with outputs 
+# Functions with outputs  
+
+def format_name(f_name,l_name):
+    print(f_name.title())
+    print(l_name.title())
+format_name("Sandra","CLEMS") #Sandra 
+# Clems
+
+# Otput with variable
+def format_name(f_name,l_name):
+    formated_f_name = (f_name.title())
+    formated_l_name = (l_name.title())
+
+    print(f"{formated_f_name}  {formated_l_name}")
+format_name("SanDRA","CLEMS") #Sandra  Clems
