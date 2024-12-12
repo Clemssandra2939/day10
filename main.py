64d435f912ca69ab5ea9ad24e4618e660da87314
Clemssandra2939 <sclems600@gmail.com>
2024-12-12 05:28:20 +0100

    First commit

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ def format_name(f_name,l_name):
     formated_f_name = (f_name.title())
     formated_l_name = (l_name.title())
 
-    print(f"{formated_f_name}  {formated_l_name}")
-format_name("SanDRA","CLEMS") #Sandra  Clems
+    print(f"){formated_f_name}  {formated_l_name}")
+format_name("SanDRA","CLEMS")#Sandra  Clems
 
-# How to use result function instead of print function 
+# How to use result function instead of print statement 
+def format_name(f_name,l_name):
+    formated_f_name = (f_name.title())
+    formated_l_name = (l_name.title())
+    return (f"{formated_f_name} {formated_l_name}")
+
+print(format_name("SanDRA","CLEMS"))
