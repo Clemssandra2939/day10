d5fd91bb74e7530b74c5404a8c7d72c6861f686e
Clemssandra2939 <sclems600@gmail.com>
2024-12-15 05:52:45 +0100

    commented out the line 1 to line 6

--- Multiple_return_values.py
+++ Multiple_return_values.py
@@ -1,6 +1,16 @@
-def format_name(f_name,l_name):
-    formated_f_name = (f_name.title())
-    formated_l_name = (l_name.title())
-    return (f"{formated_f_name} {formated_l_name}")
+# def format_name(f_name,l_name):
+#     formated_f_name = (f_name.title())
+#     formated_l_name = (l_name.title())
+#     return (f"{formated_f_name} {formated_l_name}")
 
-print(format_name("SanDRA","CLEMS"))
+# print(format_name("SanDRA","CLEMS"))
+
+# when the return keyword is excuted,it is like the defult way of saying that that line is the end of the function
+def format_name(f_name,l_name):
+    if f_name == "" or l_name == "":
+        formated_f_name = (f_name.title())
+        formated_l_name = (l_name.title())
+        return (f"{formated_f_name} {formated_l_name}")
+
+print(format_name(input(" What is yor first name?"),
+ input ("What is your last name")))
