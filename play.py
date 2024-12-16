c9461b2cf5a4e43374476fe5ecc345df43ec1125
Clemssandra2939 <sclems600@gmail.com>
2024-12-16 04:59:54 +0100

    Added leap year coding char. from day 3

---
+++ play.py
@@ -0,0 +1,35 @@
+# leap year coding char.
+# how to know whether any year is a leap year
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+
+# year=int(input("which year do u want to check?"))
+# if year % 4:
+#     print(f"{year} is a leap year")
+# elif year % 100:
+#     print(f"{year} is a not leap year")
+   
+# elif year % 400:
+#     print(f"{year} is a leap year")
+
+# else:
+#     print(f"{year} is a not leap year")
+
+# else:                                  
+   #  print("f{year} is not a leap year")
+
+
+# year=int(input("which year do u want to check? "))
+# if year % 4==0:
+#     if year % 100==0:
+
+#         if year % 400==0:
+#             print(f"{year} is leap year")
+#         else: 
+#             print(f"{year} is not leap year")
+#     else:
+#         print(f"{year} is leap year")
+
+# else:
+#     print(f"{year} is not leap year")
