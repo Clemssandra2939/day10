7e997841871c3d0c332fb91a5ce42f0a04b763f7
Clemssandra2939 <sclems600@gmail.com>
2025-01-04 05:07:41 +0100

    Return as an early exit

--- Docstrings.py
+++ Docstrings.py
@@ -3,4 +3,13 @@
 # as we are coding along in our functions or in our other blpcks of code  
 
 # Already used functions with output 
-le
+length = len(formatted_name)
+
+# Return as an early exit
+
+def format_name(f_name,l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs"
+    formated_f_name = (f_name.title())
+    formated_l_name = (l_name.title())
+    return f"Reult:{formated_f_name} {formated_l_name}"
