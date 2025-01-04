54fafb429b98b9a98c0d4a36e8e592a9d107cce8
Clemssandra2939 <sclems600@gmail.com>
2025-01-04 05:02:03 +0100

    define docstrings

---
+++ Docstrings.py
@@ -0,0 +1,6 @@
+# Docstrings
+# Docstrings are basically a way for us to create little bits of documentation 
+# as we are coding along in our functions or in our other blpcks of code  
+
+# Already used functions with output 
+le
