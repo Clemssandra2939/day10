4be532a2da71fe7b50fcc28e324961afcca9a9e9
Clemssandra2939 <sclems600@gmail.com>
2025-01-04 04:51:30 +0100

    first commit

--- Days_in_month_Exercise.py
+++ Days_in_month_Exercise.py
@@ -4,11 +4,23 @@ def is_leap(year):
     if year % 100==0:
 
      if year % 400==0:
-#             print(f"{year} is leap year")
-#         else: 
-#             print(f"{year} is not leap year")
-#     else:
-#         print(f"{year} is leap year")
+       return "True"
+     else: 
+        return "True"
+    else:
+      return "True"
+  else:
+    return "False"
 
-# else:
-#     print(f"{year} is not leap year")
+def days_in_month(year,month):
+  if month > 12 or month < 1:
+    return "No month"
+  month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+  if is_leap(year) and month == 2:
+    return 29
+  return month_days[month- 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))   
+days = days_in_month(year,month) 
+print(days) 
