78a2276aed0c5521cd6fc3189aacf0f96007c9b6
Clemssandra2939 <sclems600@gmail.com>
2025-01-06 14:20:21 +0100

    adding the functions

---
+++ Calculator.Part 1.py
@@ -0,0 +1,21 @@
+# Calculator
+
+# Add
+def add(n1,n2):
+    return n1 + n2 
+
+# Subtract
+def subtract(n1,n2):
+    return n1 - n2  
+
+# Multiply
+def multiply(n1,n2):
+    return n1 * n2  
+
+# Divide
+def divide(n1,n2):
+    return n1 / n2  
+
+# create a dictionary named operations
+# keys = symbols + - * /
+# values = the names of the functions(add,subtract,multiply,divide)
