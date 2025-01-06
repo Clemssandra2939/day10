19c6104fe8a72b80ec793c3da1f787a918152137
Clemssandra2939 <sclems600@gmail.com>
2025-01-06 15:30:43 +0100

    copied day 9 Secret auction program code

--- Calculator.Part 1.py
+++ Calculator.Part 1.py
@@ -34,3 +34,4 @@ operations ={
 
 num1 =  int(input("What's the first number? :  "))
 num2 = int(input("What's the second number?: "))
+
--- play.py
+++ play.py
@@ -104,3 +104,34 @@ travel_log = [
     },
 ]
  
+# Secret auction program
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+# finding the highest bidder
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount =bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with  bid of  ${highest_bid}")
+
+while not bidding_finished: 
+    print("Welcome to the Secret Auction Program")
+    name = input("What is your name? \n")
+    price = int(input("what's yor bid? $ \n"))
+    bids =  [name] = price 
+answer = input("Are there any other bidders?Type 'yes' or 'no'.\n")
+if  answer == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+elif answer == "yes":
+    print(logo)
+  
+    
