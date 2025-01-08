c0dcebd0c4fa049180c1485a9d6c738711652fbf
Clemssandra2939 <sclems600@gmail.com>
2025-01-08 05:35:24 +0100

    Used while loop to continue to avoid repetition

--- While.loops,flags,and,Recursion.py
+++ While.loops,flags,and,Recursion.py
@@ -37,18 +37,26 @@ operations ={
 num1 =  int(input("What's the first number? :  "))
 for symbol in operations:
     print(symbol)
+should_continue = True
 
-operation_symbol = input("Pick an operation from the line above:  ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1,num2)
+while should_continue:
+    operation_symbol = input("Pick an operation from the line above:  ")
+    num2 = int(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1,num2)
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-# To continue the calculation
-operation_symbol = input("Pick another operation from the line above:  ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1,num2),num3)
+    # if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y" :
+    #     num1 = answer
+    # else:
+    #     should_continue = False
+         
+# what if the user doesn't want to exit but want to just refresh or restart the calculation
+# How can you get back to the num 1 as to restart another calculation
+# This concept in programming is called Recursion 
 
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to refresh: ") == "y" :
+        num1 = answer
+    else:
+        should_continue = False
