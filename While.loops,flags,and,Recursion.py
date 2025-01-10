587ab4b0b5953b6f71df404ff5602812178b44d2
Clemssandra2939 <sclems600@gmail.com>
2025-01-10 06:03:08 +0100

    Starting commit

--- While.loops,flags,and,Recursion.py
+++ While.loops,flags,and,Recursion.py
@@ -33,33 +33,34 @@ operations ={
 # function(2,3)
 
 # this is how the symbols can be called
+def calculator():
+    num1 =  int(input("What's the first number? :  "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-num1 =  int(input("What's the first number? :  "))
-for symbol in operations:
-    print(symbol)
-should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above:  ")
+        num2 = int(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
 
-while should_continue:
-    operation_symbol = input("Pick an operation from the line above:  ")
-    num2 = int(input("What's the next number?: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        # if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y" :
+        #     num1 = answer
+        # else:
+        #     should_continue = False
+            
+    # what if the user doesn't want to exit but want to just refresh or restart the calculation
+    # How can you get back to the num 1 as to restart another calculation
+    # This concept in programming is called Recursion (Having a function that calls itself)
 
-    # if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y" :
-    #     num1 = answer
-    # else:
-    #     should_continue = False
-         
-# what if the user doesn't want to exit but want to just refresh or restart the calculation
-# How can you get back to the num 1 as to restart another calculation
-# This concept in programming is called Recursion 
-
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to refresh: ") == "y" :
-        num1 = answer
-    else:
-        should_continue = False
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to refresh: ") == "y" :
+            num1 = answer
+        else:
+            should_continue = False
+            calculator() #this will basically take us bk to beginning where we get to enter the new input
 
 
-        
+calculator()
